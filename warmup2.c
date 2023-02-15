da51c5ff48001d6a449a6108ad4529b49c230f7d
Barry Lin <tun32913@CIS-Linux2.temple.edu>
2023-02-15 00:36:45 -0500

    warmup2

--- warmup2.c
+++ warmup2.c
@@ -7,11 +7,16 @@
 
 
 
-
-
-
-
-
 int main(int argc, char *argv[]){
+
+    FILE *fp;
+    fp = fopen("/proc/meminfo","r");
+
+if(fp ==NULL){
+    fprintf("There has been a error with meminfo");
+    return 1;
+}
+
+
 
 }
