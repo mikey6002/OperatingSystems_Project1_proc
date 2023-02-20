029639834f6f77414bb750143041934837248e67
Barry Lin <tun32913@CIS-Linux2.temple.edu>
2023-02-19 23:19:31 -0500

    w2

--- warmup2.c
+++ warmup2.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
 
 if(fp ==NULL){
     fprintf("There has been a error with meminfo");
-    eixt(1);
+    exit(1);
 }
 char arrays [300];
 
@@ -21,8 +21,18 @@ unsigned long mem_free = 0;
 unsigned long buffers = 0;
 
 while(fgets(arrays,sizeof(arrays),fp)!=NULL){
+    if(sscanf(arrays,"Memory total: %lu kB", &mem_total)==1){
+        //total memory size 
+    }else if(sscanf(arrays,"Memory Free: %lu kB", &mem_free)==1){
+        } else if(sscanf(arrays,"Buffers : %lu kB", &buffers)==1){
 
-}
+        }
+    }
+    fclose(fp);
 
+    printf("Total memory: %lu kB\n", mem_total);
+    printf("Free memory: %lu kB\n", mem_free);
+    printf("Buffers: %lu kB\n", buffers);
 
+    return 0;
 }
